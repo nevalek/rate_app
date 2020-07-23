0a09f6343ab440bc336ae1faa9e5ad3eae64037d
hairy <nevalek.v@yandex.ru>
2020-07-23 18:49:06 +0300

    OK

---
+++ src/components/modal/Modal.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './model.scss';
+
+export const Model = ()=>{
+    return(
+        <div className='model'>
+            <div className="modelHead">
+                <ul>
+                    <li>Вход</li>
+                    <li>Регистрация</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
